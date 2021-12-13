4b92da9c2019fb6aaa90b9d0ef6fe3b70a6568bb
Radik Zangirov <owtmpatrick@gmail.com>
2021-12-13 21:56:21 +0500

    format date

--- src/store/models/Comment.ts
+++ src/store/models/Comment.ts
@@ -8,6 +8,24 @@ const Comment = types
 		date: types.Date,
 		rating: 0
 	})
+	.views(self => ({
+		timeAgo(): string {
+			const difference = new Date().getTime() - self.date.getTime();
+			const minute = 1000 * 60;
+			const hour = 1000 * 60 * 60;
+			const day = 1000 * 60 * 60 * 24;
+
+			if (difference < hour) {
+				return `${Math.ceil(difference / minute)} minutes ago`;
+			}
+
+			if (difference < day) {
+				return `${Math.ceil(difference / hour)} hours ago`;
+			}
+
+			return `${Math.ceil(difference / day)} days ago`;
+		}
+	}))
 	.actions(self => ({
 		incrementRating() {
 			self.rating += 1;
