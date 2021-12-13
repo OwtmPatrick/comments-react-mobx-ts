828d5e58d5fc7b9802d7d35848411eeddac009c8
Radik Zangirov <owtmpatrick@gmail.com>
2021-12-13 21:54:08 +0500

    edit rating

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import {styled} from '@mui/material/styles';
 import {Chip, ChipProps} from '@mui/material';
+import StyledRating from './styles';
 
-const Rating = styled((props: ChipProps) => <Chip {...props} />)(() => ({
-	marginLeft: 'auto'
-}));
+interface IProps extends ChipProps {
+	rating: number;
+}
+
+const getColor = (
+	rating: number
+): 'default' | 'primary' | 'secondary' | 'error' | 'info' | 'success' | 'warning' => {
+	if (rating < 0) {
+		return 'error';
+	}
+
+	if (rating > 0) {
+		return 'success';
+	}
+
+	return 'default';
+};
+
+const Rating = ({rating}: IProps): JSX.Element => (
+	<StyledRating>
+		<Chip label={rating} color={getColor(rating)} />
+	</StyledRating>
+);
 
 export default Rating;
