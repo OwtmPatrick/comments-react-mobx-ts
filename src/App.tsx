73a7c3f54c2732bec23636e879e23b5316580ec7
Radik Zangirov <owtmpatrick@gmail.com>
2021-12-15 21:14:53 +0500

    reset styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
 import {Provider, rootStore} from './store/models/Root';
 import CommentsList from './components/Comments-list';
 
 function App(): JSX.Element {
 	return (
 		<Provider value={rootStore}>
-			<div className="App">
-				<CommentsList />
-			</div>
+			<CssBaseline />
+
+			<CommentsList />
 		</Provider>
 	);
 }
